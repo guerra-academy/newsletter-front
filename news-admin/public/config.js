a519abb8ead9afe98ba393c32628366f41260c17
GlaucioGuerra <glaucioguerra@gmail.com>
2024-03-10 21:26:39 -0300

    refactoring frontend antigo

--- news-admin/public/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-window.configs = {
-    signInRedirectURL: "http://localhost:5173",
-    signOutRedirectURL: "http://localhost:5173/logout",
-    clientID: "5Ex8jf_szpPakraOFyTmQp0Cn7Ua",
-    baseUrl: "https://api.asgardeo.io/t/guerraacademy",
-    scope: ["openid", "profile"]
-  };
-  
